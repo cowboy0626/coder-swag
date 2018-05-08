c9471a4c568362169fc3c13726c347654d38e0ad
Kim mu hyun <cowboy0626@naver.com>
2018-05-09 01:00:22 +0900

    상품목록 컨트롤생성 및 상품셀뷰 추가 및 연결

---
+++ coder-swag/View/ProductCell.swift
@@ -0,0 +1,17 @@
+//
+//  ProductCell.swift
+//  coder-swag
+//
+//  Created by muhyunkim on 2018. 5. 9..
+//  Copyright © 2018년 muhyunkim. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+    
+}
